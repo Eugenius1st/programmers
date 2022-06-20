23f2e35b9cc58f7e2a5dc6208af52d84f3832f38
안유진 <Eugenius1st@gmail.com>
2022-06-20 12:59:22 +0900

    [#6]: 크레인 인형뽑기

--- Lv1/크레인인형뽑기게임.py
+++ Lv1/크레인인형뽑기게임.py
@@ -1,3 +1,22 @@
-def solution():
-    answer = ''
+import sys
+input = sys.stdin.readline
+
+def solution(board, moves):
+    N = len(board)
+    res = []
+    answer = 0
+    for i in moves:
+        i = i-1
+        if len(res) >= 2 and res[-1] == res[-2]:
+            res.pop()
+            res.pop()
+            answer += 2
+
+        for j in range(N):
+            if board[j][i] == 0:
+                continue
+            else:
+                res.append(board[j][i])
+                board[j][i] = 0
+                break
     return answer
