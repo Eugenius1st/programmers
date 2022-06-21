74ffe9822494440fa17d6c65d13c5c0e5e924579
안유진 <Eugenius1st@gmail.com>
2022-06-21 12:06:01 +0900

    [#8]: 해시_Lv1_완주하지 못한 선수

--- 해시/완주하지못한선수.py
+++ 해시/완주하지못한선수.py
@@ -5,7 +5,7 @@ def solution(participant, completion):
     for part in participant:
         dic[hash(part)] = part #해시값으로 dictionary에 저장
         temp += int(hash(part))
-
+    
     for com in completion:
         temp -= hash(com)
     answer = dic[temp] # 특정 해시값으로 dictionary 조회
